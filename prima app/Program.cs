fbda2c776ab26b40f30734201ed96dae6045edeb
Fnau99 <nafikri.generali@gmail.com>
2023-10-16 12:43:21 +0200

    Fork AppAuto

---
+++ prima app/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.SymbolStore;
+using System.Reflection.Metadata.Ecma335;
+
+prima_app.AutoInterattiva miaAuto;
+
+miaAuto = new prima_app.AutoInterattiva();
+
+miaAuto.StampaMenu();
+
+prima_app.Auto miaAuto2;
+
+//miaAuto2 = new prima_app.Auto();
+
+//miaAuto2.Accendi();
+//miaAuto2.StampaStato();
+
+Console.ReadLine();
+
